5698c93fc734e9ee53c06e1f46063222edbebdc2
johnnydldev <johnnydldev@gmail.com>
2024-09-16 09:27:57 -0600

    Upload of views update

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -13,6 +13,8 @@ namespace MvcMovie.Models
         public string? Genre { get; set; }
         [Column(TypeName ="decimal(18,2)")]
         public decimal Price { get; set; }
+        public string? Rating { get; set; }
+
 
     }//End movie model class
 }//End namespace
