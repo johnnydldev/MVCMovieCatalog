3486b832ecddec80d5cb815a372aa5698ac09525
johnnydldev <johnnydldev@gmail.com>
2024-09-15 21:25:51 -0600

    Upload of mvc example project

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        [HttpGet("/HelloWorld")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        //GET: /HelloWorld/Welcome/ 
+        //[HttpGet("{id}")]
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["message"] = "Hello "+name;
+            ViewData["numTimes"] = numTimes;
+            return View();
+        }
+
+    }//End hello world controller class
+}//End namespace
